615ccf123631b3c3d57b34a669fead76d5b9dce4
eiu165 <eiu165@gmail.com>
2012-05-22 13:44:47 -0500

    added css and js file

--- MvcBackbone/Global.asax.cs
+++ MvcBackbone/Global.asax.cs
@@ -24,7 +24,7 @@ namespace MvcBackbone
 			routes.MapRoute(
 				"Default", // Route name
 				"{controller}/{action}/{id}", // URL with parameters
-				new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+				new { controller = "Album", action = "Index", id = UrlParameter.Optional } // Parameter defaults
 			);
 
 		}
--- MvcBackbone/Scripts/album.js
+++ MvcBackbone/Scripts/album.js
@@ -1,1 +1,21 @@
 ﻿
+
+$(function () {
+
+    window.Album = Backbone.Model.extend({});
+
+    window.AlbumView = Backbone.View.extend({
+        tagName: 'li', 
+        className: 'album',
+        initialize: function () {
+            this.template = _.template($('#album-template').html());
+        },
+
+        render: function () {
+            var renderedContent = this.template(this.model.toJSON());
+            $(this.el).html(renderedContent);
+            return this;
+        }
+    });
+
+});
